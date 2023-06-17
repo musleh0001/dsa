9eeacc63a0e9ee401aa4d494c54434db485f0124
khan <khan.muslehuddin0001@gmail.com>
2023-06-17 21:50:14 +0600

    # :sparkles: Feature :sparkles: - adds linear search

---
+++ datastructure/array/linear_search.py
@@ -0,0 +1,23 @@
+"""
+    Linear Search is defined as a sequential search algorithm that starts at one end and goes through each
+    element of a list until the desired element is found.
+"""
+import array
+
+
+def linear_search(li: array, key: int) -> int:
+    for idx, value in enumerate(li):
+        if value == key:
+            return idx
+    return -1
+
+
+if __name__ == "__main__":
+    arr = array.array('I', [3, 10, 6, 20, 12, 8, 45, 9])
+    x = 12
+
+    index = linear_search(arr, x)
+    if index != -1:
+        print(f"Key found. Index is: {index}")
+    else:
+        print("Key not found")
