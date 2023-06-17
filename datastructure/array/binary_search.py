c9ea82c15ecfef5ddd250bd63af3977097afd4c8
khan <khan.muslehuddin0001@gmail.com>
2023-06-17 22:12:38 +0600

    :sparkles: Feature :sparkles: - binary search recursive way

--- datastructure/array/binary_search.py
+++ datastructure/array/binary_search.py
@@ -21,11 +21,26 @@ def binary_search_iter(li: array, left: int, right: int, key: int) -> int:
     return -1
 
 
+def binary_search_rec(li: array, left: int, right: int, key: int) -> int:
+    # base case
+    if left > right:
+        return -1
+
+    mid = left + (right - left) // 2
+
+    if li[mid] == key:
+        return mid
+    elif li[mid] < key:
+        return binary_search_rec(li, mid + 1, right, key)
+    else:
+        return binary_search_rec(li, left, mid - 1, key)
+
+
 if __name__ == "__main__":
     arr = array.array('I', [2, 3, 6, 7, 12, 65, 87])
     x = 7
 
-    index = binary_search_iter(arr, 0, len(arr), x)
+    index = binary_search_rec(arr, 0, len(arr), x)
     if index != -1:
         print(f"Key found at index: {index}")
     else:
